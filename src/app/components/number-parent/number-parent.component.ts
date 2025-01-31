44f3cca7f461fe63d720c8723757655736e56dc7
Tawfik-Tarek <tawfiktarek1975@gmail.com>
2025-01-31 22:41:16 +0200

    refactor number-header components; update number-parent to include new components and adjust styles

--- src/app/components/number-parent/number-parent.component.ts
+++ src/app/components/number-parent/number-parent.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NumberHeaderComponent } from "../number-header/number-header.component";
+import { TableComponent } from "../table/table.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-number-parent',
-  imports: [],
+  imports: [NumberHeaderComponent, TableComponent, FooterComponent],
   templateUrl: './number-parent.component.html',
   styleUrl: './number-parent.component.scss'
 })
