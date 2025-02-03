5ceb40c405f86ed3a9e1d7c80fe904fba1c8c4b8
Tawfik-Tarek <tawfiktarek1975@gmail.com>
2025-02-03 21:24:15 +0200

    feat(number-header): implement dropdown menu for editing and canceling number assignments

--- src/app/components/number-header/number-header.component.ts
+++ src/app/components/number-header/number-header.component.ts
@@ -1,12 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
 import { NumberHeader } from '../../models/number-data.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-number-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './number-header.component.html',
   styleUrl: './number-header.component.scss',
 })
 export class NumberHeaderComponent {
   @Input() headerData!: NumberHeader;
+  isDropdownVisible = false;
+
+  toggleDropdown(event: Event) {
+    event.stopPropagation();
+    this.isDropdownVisible = !this.isDropdownVisible;
+  }
+
+  closeDropdown() {
+    this.isDropdownVisible = false;
+  }
+
+  selectOption(option: string) {
+    this.closeDropdown();
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+    this.closeDropdown();
+  }
 }
