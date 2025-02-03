35f7b223d13a47394bb3c5d310e1dc0af69b7fa8
Tawfik-Tarek <tawfiktarek1975@gmail.com>
2025-02-03 19:54:21 +0200

    feat(footer): bind footer data to component for dynamic author and creation details

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FooterData } from '../../models/number-data.model';
 
 @Component({
   selector: 'app-footer',
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  @Input() footerData!: FooterData;
 }
